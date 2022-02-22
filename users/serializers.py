ac5415d5447ebc4b3a259777794cbc2ccdeed0a0
Andrew Smith <diver.vlz@gmail.com>
2022-02-22 19:20:00 +0300

    base API for users

---
+++ users/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+from .models import User
+
+
+class UserModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'firstname', 'lastname', 'email', )
