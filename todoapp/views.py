8c0c9208929b43be1bd5c9e2f6279d2318469854
Andrew Smith <diver.vlz@gmail.com>
2022-03-17 18:11:10 +0300

    pagination

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
