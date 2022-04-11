7c93770ca4de96e2a117207d4fdb5a5968a4d1c4
Andrew Smith <diver.vlz@gmail.com>
2022-04-11 19:21:14 +0300

    some awesome requests added

--- todo_notice/schema.py
+++ todo_notice/schema.py
@@ -8,7 +8,7 @@ from todoapp.models import Project, ToDo
 class UserType(DjangoObjectType):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_staff']
 
 
 class ProjectType(DjangoObjectType):
@@ -28,6 +28,12 @@ class Query(graphene.ObjectType):
     all_projects = graphene.List(ProjectType)
     all_todos = graphene.List(TodoType)
 
+    user_by_id = graphene.Field(UserType, id=graphene.ID(required=True))
+    project_by_name = graphene.Field(ProjectType, name=graphene.String(required=True))
+    todo_by_project_id = graphene.Field(TodoType, id=graphene.ID(required=True))
+    user_projects = graphene.List(ProjectType, id=graphene.ID(required=True))
+    notes_by_user_name = graphene.Field(TodoType, username=graphene.String(required=True))
+
     def resolve_all_users (root, info):
         return User.objects.all()
 
@@ -36,6 +42,27 @@ class Query(graphene.ObjectType):
 
     def resolve_all_todos (root, info):
         return ToDo.objects.all()
+
+    def resolve_user_by_id(root, info, id):
+        try:
+            return User.objects.get(id=id)
+        except User.DoesNotExist:
+            return None
+
+    def resolve_project_by_name(self, info, name):
+        try:
+            return Project.objects.get(project_name=name)
+        except Project.DoesNotExist:
+            return None
+
+    def resolve_todo_by_project_id(self, info, id):
+        return ToDo.objects.get(project__id=id)
+
+    def resolve_user_projects(root, info, id):
+        return Project.objects.filter(workers__id=id)
+
+    def resolve_notes_by_user_name(root, info, username):
+        return ToDo.objects.get(created_by__username=username)
 
 
 schema = graphene.Schema(query=Query)
